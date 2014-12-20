bc1a5859c1b15d6c5ba7661ede0ca26d4741faa0
Rostyslav Rava <rostyslav@ravasoft.com>
2014-12-20 16:44:21 +0200

    Updated tables DDL to match maximum possible email  address length (http://en.wikipedia.org/wiki/Email_address#Syntax) and unicode domain names.

--- app/common/db.php
+++ app/common/db.php
@@ -15,8 +15,8 @@ define('ORDERS_TABLE', 'pporders');
  */
 function getConnection() {
 	
-	$usersTableCreateQuery = "CREATE TABLE IF NOT EXISTS `ppusers` (`user_id` int(11) NOT NULL AUTO_INCREMENT,  `email` varchar(20) DEFAULT NULL,  `password` varchar(50) DEFAULT NULL,  `creditcard_id` varchar(40) DEFAULT NULL,  PRIMARY KEY (`user_id`),  UNIQUE KEY `email` (`email`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
-	$ordersTableCreateQuery = "CREATE TABLE IF NOT EXISTS `pporders` (`order_id` int(11) NOT NULL AUTO_INCREMENT,  `user_id` varchar(20) DEFAULT NULL,  `payment_id` varchar(50) DEFAULT NULL,  `state` varchar(20) DEFAULT NULL,  `amount` varchar(20) DEFAULT NULL,  `description` varchar(40) DEFAULT NULL,  `created_time` datetime DEFAULT NULL,   PRIMARY KEY (`order_id`)) ENGINE=InnoDB;";
+	$usersTableCreateQuery = "CREATE TABLE IF NOT EXISTS `ppusers` (`user_id` int(11) NOT NULL AUTO_INCREMENT,  `email` varchar(254) DEFAULT NULL,  `password` varchar(50) DEFAULT NULL,  `creditcard_id` varchar(40) DEFAULT NULL,  PRIMARY KEY (`user_id`),  UNIQUE KEY `email` (`email`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+	$ordersTableCreateQuery = "CREATE TABLE IF NOT EXISTS `pporders` (`order_id` int(11) NOT NULL AUTO_INCREMENT,  `user_id` varchar(254) DEFAULT NULL,  `payment_id` varchar(50) DEFAULT NULL,  `state` varchar(20) DEFAULT NULL,  `amount` varchar(20) DEFAULT NULL,  `description` varchar(40) DEFAULT NULL,  `created_time` datetime DEFAULT NULL,   PRIMARY KEY (`order_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 
 	$link = @mysql_connect(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD);
 	if(!$link) {
